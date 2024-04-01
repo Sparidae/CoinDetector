d4e86fbc82d5820e6fae1ed9723215b050eb9a84
Sparidae <2992646478@qq.com>
2024-04-01 14:34:39 +0800

    feat: time

--- methods_numba.py
+++ methods_numba.py
@@ -1,38 +1,29 @@
-import logging
-
 import cv2
 
 # import matplotlib
 # matplotlib.use("tkagg")
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.ndimage.filters import convolve as conv2d
-from tqdm import tqdm
-
-logging.basicConfig(
-    filename="exp.log",
-    filemode="w",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(funcName)s - %(message)s",
-)
+from numba import jit
 
 
-def canny(
+@jit(cache=True)
+def conv2d(input, weights, mode="constant"):
+    h, w = input.shape
+    output = np.zeros((h, w), dtype=np.int16)
+    wh, ww = weights.shape
+
+    return output
+
+
+@jit(cache=True)
+def canny_numba(
     image,
     high_threshold: int = 200,
     low_threshold: int = 100,
     debugging=True,
     quiet=False,
 ):
-    """
-    给出numpy数组图像，和迟滞阈值法两个阈值
-    """
-    # 展示原图性质
-    if debugging and not quiet:
-        show_img(image, "image")
-    elif not quiet:
-        show_img(image, "image", analysis=False)
-
     # ----------------------------------------高斯滤波,默认5*5滤波
     # 输入image
     gaussian_kernel = np.array(
@@ -46,26 +37,21 @@ def canny(
         dtype=np.float32,
     )  # 使用固定大小的kernel
     gaussian_kernel = gaussian_kernel / 159.0
-    # TODO 改成生成不同大小的高斯kernel做滤波
 
-    if debugging and not quiet:
-        print(gaussian_kernel)
     h, w = image.shape
-    if not quiet:
-        print(f"image shape:{image.shape}")
 
-    # 应用高斯平滑
+    # 应用高斯平滑 手动实现
     image_smoothed = conv2d(
         input=image,
         weights=gaussian_kernel,
         mode="constant",
     )
-    logging.info("高斯平滑完成")
+    # logging.info("高斯平滑完成")
 
     # show_img(image_smoothed, "image_smoothed")
     image_smoothed = np.float32(image_smoothed)
-    if debugging and not quiet:
-        show_img(image_smoothed, "image_smoothed")
+    # if debugging and not quiet:
+    #     show_img_numba(image_smoothed, "image_smoothed")
 
     # ----------------------------------------sobel梯度幅值
     # 现在有image_smmothed
@@ -84,18 +70,18 @@ def canny(
         sobel_y,
         mode="constant",
     )
-    if debugging and not quiet:
-        show_img(grad_x, "grad_x")
-        show_img(grad_y, "grad_y")
+    # if debugging and not quiet:
+    #     show_img_numba(grad_x, "grad_x")
+    #     show_img_numba(grad_y, "grad_y")
     grad = np.hypot(
         grad_x, grad_y
     )  # 计算对应元素的梯度均值，hypot相当于直角三角形求斜边
-    logging.info("计算梯度完成")
+    # logging.info("计算梯度完成")
 
     # print(grad.max())
     grad = grad / grad.max() * 255  # 整理到灰度区间内
-    if debugging and not quiet:
-        show_img(grad, "grad")
+    # if debugging and not quiet:
+    #     show_img_numba(grad, "grad")
 
     direction = np.arctan2(
         grad_y, grad_x
@@ -105,9 +91,9 @@ def canny(
         np.pi
     )  # 将角度统一到正区间，减少方向判断数量（不能用绝对值，虽然不影响边缘检测，但是影响霍夫圆识别
 
-    if debugging and not quiet:
-        analyze_array(direction, "direction")
-    logging.info("计算梯度方向完成")
+    # if debugging and not quiet:
+    #     analyze_array(direction, "direction")
+    # logging.info("计算梯度方向完成")
 
     # ----------------------------------------非极大值抑制NMS
     # 现在有grad,direction
@@ -116,7 +102,7 @@ def canny(
     grad_nms = np.zeros((h, w), dtype=np.int32)
 
     # 计算nms，只保留每个像素梯度方向的像素
-    for x in tqdm(range(1, h - 1), desc="nms calculating"):
+    for x in range(1, h - 1):
         for y in range(1, w - 1):
             # 临近像素p 设置为255是因为避免在没有进入分支的情况下被误分
             p1, p2 = 256, 256
@@ -143,10 +129,10 @@ def canny(
             else:
                 grad_nms[x, y] = 0
 
-    logging.info("计算NMS完成")
+    # logging.info("计算NMS完成")
     grad_nms = grad_nms / grad_nms.max() * 255
-    if debugging and not quiet:
-        show_img(grad_nms, "grad_nms")
+    # if debugging and not quiet:
+    #     show_img_numba(grad_nms, "grad_nms")
 
     # ----------------------------------------迟滞阈值 门限法
     # 输入 grad_nms
@@ -163,17 +149,22 @@ def canny(
 
     # TODO 实现弱边缘检测
 
-    if debugging and not quiet:
-        show_img(edge, "edge")
-    elif not quiet:
-        show_img(edge, "edge", analysis=False)
+    # if debugging and not quiet:
+    #     show_img_numba(edge, "edge")
+    # elif not quiet:
+    #     show_img_numba(edge, "edge", analysis=False)
 
     # print(image.shape, edge.shape, direction.shape) #这行代表处理前后形状不改变
 
     return edge, direction
 
 
-def hough_circle(
+@jit(
+    cache=True,
+    forceobj=True,
+    looplift=True,
+)
+def hough_circle_numba(
     image,  # 未经处理的图像
     high_threshold=200,  # 边缘高阈值 最大为255
     low_threshold=100,  # 边缘低阈值
@@ -188,7 +179,7 @@ def hough_circle(
     quiet=False,  # 安静，只展示结果，该选项设置为True会屏蔽上述调试选项
 ):
     # 使用canny提取边缘，得到的是int32类型的图像数组最大255
-    edge, direction = canny(
+    edge, direction = canny_numba(
         image,
         high_threshold=high_threshold,
         low_threshold=low_threshold,
@@ -204,7 +195,7 @@ def hough_circle(
 
     # 投票,每个点投出一个圆锥的两条母线
     x, y = np.where(edge > 250)
-    for i in tqdm(range(len(x)), desc="voting"):
+    for i in range(len(x)):
         # 遍历所有的边缘点
         for r in range(min_r, max_r):
             # 遍历所有的半径，投票
@@ -221,33 +212,26 @@ def hough_circle(
 
     # 抑制圆心
     # analyze_array(vote_space[vote_space > 10], "vote space")  # 检查形状
-    if debugging and not quiet:  # 分析圆心分布,按照票数从低到高
-        for i in range(2, 20, 3):
-            analyze_array(vote_space[vote_space > i], "vote space")
+    # if debugging and not quiet:  # 分析圆心分布,按照票数从低到高
+    #     for i in range(2, 20, 3):
+    #         analyze_array(vote_space[vote_space > i], "vote space")
 
     # 筛选出可能的候选圆心（调试方法，
     circles = []
-    if vt_debugging and not quiet:
-        """
-        因为发现筛选圆心的过程时间代价比较小，
-        所以可以尝试使用不同的最小投票数量来画出不同的圆，找到最适合的圆
-        """
-        for v in range(5, np.max(vote_space), vtd_step):
-            print(f"voting threshold:{v}")
-            circles = _center_combine(vote_space, v, min_center_distance)
-            _draw_circle(image, circles)
-            cl = [c[2] for c in circles]  # 找到最大值
-            print(f"Above circles: min_r{min(cl)},max_r{max(cl)}")
-            print("-" * 100)
-    else:
-        circles = _center_combine(vote_space, min_voting, min_center_distance)
-        _draw_circle(image, circles, quiet)
+
+    circles = _center_combine_numba(vote_space, min_voting, min_center_distance)
+    _draw_circle(image, circles)
 
     # print(circles)  # 打印所有的圆坐标和半径
-    return circles
+    return image, circles
 
 
-def _center_combine(
+@jit(
+    cache=True,
+    forceobj=True,
+    looplift=False,
+)
+def _center_combine_numba(
     vote_space,
     min_voting,
     min_dist,
@@ -260,14 +244,38 @@ def _center_combine(
     circles_to_be_selected = np.stack(
         circles_to_be_selected, axis=1
     )  # 返回圆心[x,y,r]组成的列表
+
     # circles_to_be_selected = np.array(circles_to_be_selected)
-    # 判断圆心之间的距离，如果小于最小圆心距离就当作是一个圆
-    # 相对比较笨的方法，双层循环
     selected_label = np.zeros(
         len(circles_to_be_selected), dtype=np.int8
     )  # 已经选中的圆标记为1
     circles = []  # 存放已经找到的圆
-    for i, centeri in tqdm(enumerate(circles_to_be_selected), desc="filter circle"):
+    # for i, centeri in enumerate(circles_to_be_selected):
+    #     # 对于每一个待选的圆，找到和其他圆一样的点
+    #     # 如果选中的店已经被标记为圆心
+    #     if selected_label[i] > 0:
+    #         continue
+    #     selected_label[i] += 1  # 圆心选中，标记
+    #     # 圆心列表,同一个圆心
+    #     same_center = []
+    #     same_center.append(centeri)
+    #     x, y, r = np.mean(same_center, axis=0)  # 列表套数组可以求,双列表也可以求
+    #     for j, centerj in enumerate(circles_to_be_selected):
+    #         if selected_label[j] > 0:
+    #             continue
+    #         # 判断距离是否小于阈值，小于则归为一个圆
+    #         if (x - centerj[0]) ** 2 + (y - centerj[1]) ** 2 < min_dist**2:
+    #             # 同一个圆
+    #             same_center.append(centerj)
+    #             x, y, r = np.mean(
+    #                 same_center, axis=0
+    #             )  # 取圆心列表所有点的平均值作为中心点
+    #             # 标记
+    #             selected_label[j] += 1
+
+    #     circles.append([x, y, r])
+
+    for i in range(circles_to_be_selected.shape[0]):
         # 对于每一个待选的圆，找到和其他圆一样的点
         # 如果选中的店已经被标记为圆心
         if selected_label[i] > 0:
@@ -275,15 +283,18 @@ def _center_combine(
         selected_label[i] += 1  # 圆心选中，标记
         # 圆心列表,同一个圆心
         same_center = []
-        same_center.append(centeri)
+        same_center.append(circles_to_be_selected[i])
         x, y, r = np.mean(same_center, axis=0)  # 列表套数组可以求,双列表也可以求
-        for j, centerj in enumerate(circles_to_be_selected):
+        for j in range(len(circles_to_be_selected)):
             if selected_label[j] > 0:
                 continue
             # 判断距离是否小于阈值，小于则归为一个圆
-            if (x - centerj[0]) ** 2 + (y - centerj[1]) ** 2 < min_dist**2:
+            if (
+                (x - circles_to_be_selected[j][0]) ** 2
+                + (y - circles_to_be_selected[j][1]) ** 2
+            ) < min_dist**2:
                 # 同一个圆
-                same_center.append(centerj)
+                same_center.append(circles_to_be_selected[j])
                 x, y, r = np.mean(
                     same_center, axis=0
                 )  # 取圆心列表所有点的平均值作为中心点
@@ -314,10 +325,10 @@ def _draw_circle(img, circles, quiet=False):
         plt.tight_layout()
         plt.show()
     else:
-        show_img(img_c, analysis=False)
+        show_img_numba(img_c, analysis=False)
 
 
-def show_img(image, title="", analysis=True):
+def show_img_numba(image, title="", analysis=True):
     """
     展示灰度图片,并可选的分析像素分布
     """
@@ -382,4 +393,6 @@ if __name__ == "__main__":
     img_path = "./img/coins5.jpg"
     # test hough
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    image, circles = hough_circle(img, **config[img_path])
+    image, circles = hough_circle_numba(img, **config[img_path])
+    print(circles)
+    # _draw_circle(image, circles)
